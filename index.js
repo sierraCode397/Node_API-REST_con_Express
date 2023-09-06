f07b35a1546d02033cb3245221af7c2051b0fe3b
izaack107 <izaack107@gmail.com>
2023-09-06 08:19:26 -0600

    Instalacion de express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("My app");
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) =>{
+  res.send('Hola, es mi server de express ');
+});
+
+app.listen(port, () => {
+  console.log(`Mi port 3000 Listening at http://localhost:${port}`);
+});
