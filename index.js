920b93ee5bafcc9fae7e449c7d4de5effa6a9da1
izaack107 <izaack107@gmail.com>
2023-09-06 08:52:33 -0600

    Routing con express

--- index.js
+++ index.js
@@ -6,6 +6,26 @@ app.get('/', (req, res) =>{
   res.send('Hola, es mi server de express ');
 });
 
+app.get('/nueva-ruta', (req, res) =>{
+  res.send('Hola, Soy una nueva ruta o endPoint');
+});
+
+app.get('/productos', (req, res) =>{
+  res.json({
+    name: 'Product 1',
+    price: 1000,
+  });
+});
+
+app.get('/categoria', (req, res) =>{
+  res.json({
+    1: 'Electronicos',
+    2: 'Shose',
+    3: 'Artesanal'
+
+  });
+});
+
 app.listen(port, () => {
   console.log(`Mi port 3000 Listening at http://localhost:${port}`);
 });
